4458ad3227df555c608dcc944b495784a9f3613d
Kozo Nishida <kozo.nishida@gmail.com>
2023-06-08 13:22:26 +0900

    Obtain the Quantitative Value Matrix

--- R/msdial2SummarizedExperiment.R
+++ R/msdial2SummarizedExperiment.R
@@ -1,4 +1,14 @@
 importMsdialTable <- function(tablefile){
-  tbl <- read_delim(tablefile, delim = "\t", escape_double = FALSE,
-                col_names = FALSE, trim_ws = TRUE)
+    tbl <- readr::read_delim(tablefile, delim = "\t", escape_double = FALSE,
+                             col_names = FALSE, trim_ws = TRUE)
+    numof_peakmetadata_cols <- 29
+    quantval_cols <- tbl %>% dplyr::select(numof_peakmetadata_cols:ncol(tbl))
+    numof_sample_class <- length(na.omit(unique(unlist(quantval_cols[1,]))))
+    numof_avgstdev_cols <- numof_sample_class * 2
+    quantval_cols <- quantval_cols %>%
+        dplyr::select(1:(ncol(quantval_cols)-numof_avgstdev_cols))
+
+    sample_meta_tbl <- quantval_cols[c(1,2,3,4,5),]
+    quantval_tbl <- quantval_cols[-c(1,2,3,4,5),]
+
 }
